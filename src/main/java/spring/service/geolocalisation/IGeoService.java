392c7d98b30ba44c14b332d75237d6cf34ad174b
Jakub Kołecki <jakubkolecki93@gmail.com>
2016-06-18 18:33:42 +0200

    servis geolokalizacyjny rozpoczęcie prac

---
+++ src/main/java/spring/service/geolocalisation/IGeoService.java
@@ -0,0 +1,7 @@
+package spring.service.geolocalisation;
+
+/**
+ * Created by jakub on 17.06.16.
+ */
+public interface IGeoService {
+}
---
+++ src/main/java/spring/service/geolocalisation/impl/GeoServiceImpl.java
@@ -0,0 +1,9 @@
+package spring.service.geolocalisation.impl;
+
+import spring.service.geolocalisation.IGeoService;
+
+/**
+ * Created by jakub on 17.06.16.
+ */
+public class GeoServiceImpl implements IGeoService {
+}
