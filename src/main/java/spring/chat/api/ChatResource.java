2529e8ed9413c07c7d82aae7113b4a6a41654efe
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2016-06-19 12:16:23 +0200

    New rests for Chat Service: -sending messages -getting messages, -deactivaton messages,

---
+++ src/main/java/spring/chat/api/ChatResource.java
@@ -0,0 +1,63 @@
+package spring.chat.api;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import spring.chat.dto.ChatCreateDTO;
+import spring.chat.dto.ChatDTO;
+import spring.chat.service.ChatService;
+
+import javax.inject.Inject;
+import java.util.List;
+
+/**
+ * Created by Tomasz Komoszeski on 2016-06-18.
+ */
+
+@RestController
+@RequestMapping("/api")
+public class ChatResource {
+
+    private final Logger log = LoggerFactory.getLogger(ChatResource.class);
+
+    @Inject
+    ChatService chatService;
+
+    @RequestMapping(value = "/chat",
+            method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<ChatDTO> sendMessage(@RequestBody ChatCreateDTO chatCreateDTO){
+        return new ResponseEntity<>(chatService.sendMessage(chatCreateDTO),HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/chat/{recevier:[_'.@a-z0-9-]+}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<List<ChatDTO>> getMyMessagesTo(@PathVariable("recevier") String aRecevier){
+        return new ResponseEntity<>(chatService.getMessagesByRecevier(aRecevier),HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/chat/",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<List<ChatDTO>> getAllMyMessages(){
+        return new ResponseEntity<>(chatService.getAllMessages(),HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/chat/{id}",
+            method = RequestMethod.POST
+    )
+    public ResponseEntity<Void> deactivateMessage(@PathVariable("id") Long aId){
+        chatService.deactivateMessage(aId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
